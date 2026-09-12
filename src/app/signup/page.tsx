bf2ca91ff5d5d0a2ced13183acb7b80adc19c64c
Jagrut Garg <jagrutgarg61@gmail.com>
2026-09-12 17:19:32 +0530

    Fix emailRedirectTo in signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -64,7 +64,7 @@ export default function SignupPage() {
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/auth/callback`,
+          emailRedirectTo: `${window.location.origin}`,
           data: {
             first_name: firstName,
             last_name: lastName,
